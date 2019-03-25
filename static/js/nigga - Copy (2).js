248f0f1c93e482e114096efd6d62fe4bd907fa0f
olimpija <obrad.s031@gmail.com>
2019-03-25 17:56:37 +0100

    big changes 2

--- static/js/nigga - Copy (2).js
+++ static/js/nigga - Copy (2).js
@@ -38,39 +38,3 @@ $(document).ready(function() {
     // });
 
   });
-
-
-  // $(function() {
-  //   var $el,
-  //     leftPos,
-  //     newWidth,
-  //     $mainNav = $(".navbar-nav");
-  
-  //   $mainNav.append("<li class='nav-item' id='magic-line'></li>");
-  //   var $magicLine = $("#magic-line");
-  
-  //   $magicLine
-  //     .width($(".active").width())
-  //     .css("left", $(".active a").position().left)
-  //     .data("origLeft", $magicLine.position().left)
-  //     .data("origWidth", $magicLine.width());
-  
-  //   $(".navbar-nav li a").hover(
-  //     function() {
-  //       $el = $(this);
-  //       leftPos = $el.position().left;
-  //       newWidth = $el.parent().width();
-  //       $magicLine.stop().animate({
-  //         left: leftPos,
-  //         width: newWidth
-  //       });
-  //     },
-  //     function() {
-  //       $magicLine.stop().animate({
-  //         left: $magicLine.data("origLeft"),
-  //         width: $magicLine.data("origWidth")
-  //       });
-  //     }
-  //   );
-  // });
-  
